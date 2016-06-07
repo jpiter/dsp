0c0c577ffc17d78627e5dab18dfdcf0b199e5046
jpiter <jpiter@users.noreply.github.com>
2016-06-06 22:51:22 -0400

    Updated q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -2,12 +2,27 @@
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+
+ds = datetime.datetime.strptime(date_start,"%m-%d-%Y")
+de = datetime.datetime.strptime(date_stop,"%m-%d-%Y")
+diff = de-ds
+print("The difference is ",diff.days," days")
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+ds = datetime.datetime.strptime(date_start,"%m%d%Y")
+de = datetime.datetime.strptime(date_stop,"%m%d%Y")
+diff = de-ds
+print("The difference is ",diff.days," days")
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+ds = datetime.datetime.strptime(date_start,"%d-%b-%Y")
+de = datetime.datetime.strptime(date_stop,"%d-%b-%Y")
+diff = de-ds
+print("The difference is ",diff.days," days")

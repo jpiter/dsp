32c70641a8b8a825ce8622f9eaf00658dfdf09a8
jpiter <jpiter@users.noreply.github.com>
2016-06-07 23:19:19 -0400

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -7,7 +7,6 @@ def match_ends(words):
     Given a list of strings, return the count of the number of strings
     where the string length is 2 or more and the first and last chars
     of the string are the same.
-
     >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
     3
     >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
@@ -15,16 +14,23 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
+    count = 0
+    
+    for x in words:
+        if len(x)>=2 and x[0]==x[-1]:
+            count += 1
+    
+    return count
+    
     raise NotImplementedError
-
-
-def front_x(words):
+    
+    
+    def front_x(words):
     """
     Given a list of strings, return a list with the strings in sorted
     order, except group all the strings that begin with 'x' first.
     e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
-
     >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
     ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
     >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
@@ -32,16 +38,22 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
+    words.sort()
+    
+    words.sort(key = lambda x: x[0]=='x', reverse=True)
+    
+    return words
+    
     raise NotImplementedError
-
-
-def sort_last(tuples):
+    
+    
+    
+    def sort_last(tuples):
     """
     Given a list of non-empty tuples, return a list sorted in
     increasing order by the last element in each tuple.
     e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
          [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
     >>> sort_last([(1, 3), (3, 2), (2, 1)])
     [(2, 1), (3, 2), (1, 3)]
     >>> sort_last([(2, 3), (1, 2), (3, 1)])
@@ -49,16 +61,20 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
+    tuples.sort(key = lambda x: x[-1])    
+    
+    return tuples
+    
     raise NotImplementedError
-
-
-def remove_adjacent(nums):
+    
+    
+    
+    def remove_adjacent(nums):
     """
     Given a list of numbers, return a list where all adjacent equal
     elements have been reduced to a single element, so [1, 2, 2, 3]
     returns [1, 2, 3]. You may create a new list or modify the passed
     in list.
-
     >>> remove_adjacent([1, 2, 2, 3])
     [1, 2, 3]
     >>> remove_adjacent([2, 2, 3, 3, 3])
@@ -68,16 +84,28 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
+    if nums==[]:
+        return nums
+    else:
+        new_nums = [nums[0]]
+    
+    for x in nums[1:]:
+        
+        if new_nums[-1]!=x:
+            new_nums.append(x)
+            
+    return new_nums
+        
     raise NotImplementedError
-
-
-def linear_merge(list1, list2):
+    
+    
+    
+    def linear_merge(list1, list2):
     """
     Given two lists sorted in increasing order, create and return a
     merged list of all the elements in sorted order. You may modify
     the passed in lists. Ideally, the solution should work in "linear"
     time, making a single pass of both lists.
-
     >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
     ['aa', 'bb', 'cc', 'xx', 'zz']
     >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
@@ -85,4 +113,27 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
+    
+    """
+    This program for each value in the list2, figures out where to 
+    insert it by effectively comparing with elements in list1
+    """
+    pos = 0
+    
+    for y in list2:
+        flag = 0 # flag changes to 1, once y is inserted  in list1
+        i = pos # starting position in list1 to which compare y values
+        while flag == 0 and i<len(list1):
+            if y < list1[i]:
+                list1.insert(i,y)
+                pos = i + 1  #next y will be first compared to the same list1 value
+                flag = 1  #indicates y has been inserted
+            else:
+                i += 1  #move to the next value in list1
+    #if y is larger than any value in list1, append the rest of the list2 to list1
+        if flag == 0:
+            list1.extend(list2[list2.index(y):]) 
+            return list1
+    return list1
+                    
     raise NotImplementedError
